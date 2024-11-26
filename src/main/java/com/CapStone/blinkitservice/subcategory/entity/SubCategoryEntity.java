34dd8852624826749ebf660a6bbb5ef22242af68
Ayush Nigam <ayushnigam1310@gmail.com>
2024-11-26 21:16:57 +0530

    some changes made - removed unused dto and entity and added subcategory title in the response dto

--- src/main/java/com/CapStone/blinkitservice/subcategory/entity/SubCategoryEntity.java
+++ src/main/java/com/CapStone/blinkitservice/subcategory/entity/SubCategoryEntity.java
@@ -9,21 +9,15 @@ import lombok.experimental.FieldDefaults;
 @Entity
 @Table(name="sub_categories")
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SubCategoryEntity {
 
     @Id
-    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
-    @Column(name = "title", nullable = false, unique = true)
     String title;
 
-    @Column(name = "image_url")
     String imageUrl;
 
     @ManyToOne
