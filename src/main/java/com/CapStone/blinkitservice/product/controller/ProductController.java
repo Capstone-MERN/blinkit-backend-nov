eef385f216b70816ede00e92d000520b40543e30
Ayush Nigam <ayushnigam1310@gmail.com>
2024-12-13 20:10:24 +0530

    cart quantity and price issue fixed

--- src/main/java/com/CapStone/blinkitservice/product/controller/ProductController.java
+++ src/main/java/com/CapStone/blinkitservice/product/controller/ProductController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,26 +20,27 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/search")
-    public ResponseEntity<ProductSearchResponseDto> searchProducts(Pageable pageable, @RequestBody ProductSearchRequestDto productSearchRequestDto){
+    public ResponseEntity<ProductSearchResponseDto> searchProducts(Pageable pageable, @RequestBody ProductSearchRequestDto productSearchRequestDto,
+                                                                   @AuthenticationPrincipal String userEmail){
 
         ProductSearchResponseDto productSearchResponseDto;
         if(productSearchRequestDto.getQuery() != null){
-            productSearchResponseDto = productService.querySearch(productSearchRequestDto.getQuery(), pageable);
+            productSearchResponseDto = productService.querySearch(productSearchRequestDto.getQuery(), pageable, userEmail);
         }else {
             productSearchResponseDto = productService.categorySearch(productSearchRequestDto.getCategoryId(),
-                    productSearchRequestDto.getSubCategoryId(), productSearchRequestDto.getFilter(), pageable);
+                    productSearchRequestDto.getSubCategoryId(), productSearchRequestDto.getFilter(), pageable, userEmail);
         }
 
         return new ResponseEntity<>(productSearchResponseDto, HttpStatus.OK);
     }
 
     @GetMapping("details")
-    public ResponseEntity<Object> productDetail(@RequestParam int id){
+    public ResponseEntity<Object> productDetail(@RequestParam int id, @AuthenticationPrincipal String userEmail){
 
         ProductDetailResponseDto productDetailResponseDto;
 
         try {
-            productDetailResponseDto = productService.productDetail(id);
+            productDetailResponseDto = productService.productDetail(id, userEmail);
 
             return new ResponseEntity<>(productDetailResponseDto, HttpStatus.OK);
         } catch (Exception e){
